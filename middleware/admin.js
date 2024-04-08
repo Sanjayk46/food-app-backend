6dca0a9b8a89e48dda14b8f1cdcb8d0c5df6f718
Sanjayk46 <sanjayks8046@gmail.com>
2024-04-08 21:56:03 +0530

    Merge remote changes

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,20 +1,3 @@
-// const { verify } = require('jsonwebtoken');
-// const UNAUTHORIZED = 401;
-// const admin = (req, res, next) => {
-//   const adminHeader = req.headers['authorization'];
-//   const token = adminHeader.split(' ')[1];
-//   // const token = req.headers.access_token;
-//   if (!token) return res.status(UNAUTHORIZED).send();
-
-//   try {
-//     const decoded = verify(token, process.env.JWT_SECRET);
-//     req.user = decoded;
-//     next();
-//   } catch (error) {
-//     res.status(UNAUTHORIZED).send();
-//   }
-// };
-//module.exports = admin;
 const UNAUTHORIZED = 400;
 const authMiddleware = require("./auth");
 const admin = (req, res, next) => {
